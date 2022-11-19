1d193836215fafabc5ef399dd993d53b3a63e36b
NathanDV <103887913+Nathan-DV@users.noreply.github.com>
2022-11-19 17:32:43 +0000

    More

---
+++ src/utils/event.ts
@@ -0,0 +1,17 @@
+import { Event, EventExec, EventKeys } from '../types'
+import { Client } from 'discord.js'
+
+export const event = <T extends EventKeys>(id: T, exec: EventExec<T>): Event<T> => {
+    return {
+        id,
+        exec
+    }
+}
+
+export const registerEvents = (client: Client, events: Event<any>[]): void =>  {
+    for (const event of events)
+        client.on(event.id, event.exec.bind(null, {
+            client,
+            log: (...args) => console.log(`[${event.id}]`, ...args)
+        }))   
+}

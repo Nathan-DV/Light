c5047192b30994653dfaf944cbcabbe360214f5b
NathanDV <103887913+Nathan-DV@users.noreply.github.com>
2022-11-19 18:41:02 +0000

    More

--- src/commands/debug/ping.ts
+++ src/commands/debug/ping.ts
@@ -10,7 +10,7 @@ export default command(meta, ({ interaction }) => {
         ephemeral: false,
         embeds: [{
             title: ":ping_pong: Pong!",
-            description: `WS: ${Math.round(interaction.client.ws.ping)} MS`,
+            description: `**WS:** \`${Math.round(interaction.client.ws.ping)} MS\``,
             color: 0x51AEFF
         }]
     })

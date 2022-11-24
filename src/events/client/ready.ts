d4d22ddb19d690045dd760882c6666e461531eba
NathanDV <103887913+Nathan-DV@users.noreply.github.com>
2022-11-24 17:50:46 +0000

    Restart

--- src/events/client/ready.ts
+++
@@ -1,5 +0,0 @@
-import { event } from '../../structures/event'
-
-export default event('ready', ({ log }, client) => {
-    log(`Logged in as ${client.user.tag}`)
-})
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv'
-import { resolve } from 'path'
-
-import ExtendedClient from "./structures/client"
-
-config({ path: resolve(__dirname, '..', ".env") })
-new ExtendedClient().init(process.env.CLIENT_TOKEN as string)

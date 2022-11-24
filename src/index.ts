19b914df53ac80d1cd4f3f4f81f46f5e44f4cb77
NathanDV <103887913+Nathan-DV@users.noreply.github.com>
2022-11-24 18:08:06 +0000

    More

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import { config } from 'dotenv'
+import { resolve } from 'path'
+
+config({ path: resolve(__dirname, '..', '.env') })
+
 import { ExtendedClient } from "./structures/client";
 
 new ExtendedClient().init();
